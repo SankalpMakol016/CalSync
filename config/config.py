04971d49a65e47da6fe23258aa3aee58c66224ff
SankalpMakol016 <sankalp.sumit.makol@gmail.com>
2026-07-13 01:17:38 +0530

    dockerized CalSync

--- config/config.py
+++ config/config.py
@@ -5,6 +5,9 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
+    
+    FLASK_ENV = os.getenv("FLASK_ENV", "production")
+    FLASK_DEBUG = os.getenv("FLASK_DEBUG", "False").lower() == "true"
 
     MYSQL_HOST = os.getenv("MYSQL_HOST")
     MYSQL_PORT = int(os.getenv("MYSQL_PORT"))
